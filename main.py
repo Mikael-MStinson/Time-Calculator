13e6ead2d12625b447c34f012d58f8547e726c11
Mikael Morrell-Stinson <67693304+Mikael-MStinson@users.noreply.github.com>
2020-11-25 11:50:57 -0600

    Added Tokenizer and Parser for Time Entries
    token_times_from_string takes in the string and returns a list of tokens, integers representing times, and a boolean value denoting if it's chargable or not.
    
    parse_time takes in the tokens and sums up the total amount of time listed. The extra argument match_type can be left as None to count all times, or be set to True or False to count deductable or non-deductable times only.

--- main.py
+++ main.py
@@ -1,9 +1,51 @@
+import re
+
 def calculate_time(start,end):
-    if end < start:
-        end += 1200
-    #slope = 10/6 
-    start_minute = start%100
-    end_minute = end%100
-    start = start - start_minute + (start_minute * (10/6))
-    end = end - end_minute + (end_minute * (10/6))
-    return round((end-start)/100,2)
+	if end < start:
+		end += 1200
+	#slope = 10/6 
+	start_minute = start%100
+	end_minute = end%100
+	start = start - start_minute + (start_minute * (10/6))
+	end = end - end_minute + (end_minute * (10/6))
+	return round((end-start)/100,2)
+	
+def token_times_from_string(string):
+	if string == "":
+		return []
+	int_pattern = re.compile(r"\d{3,4}")
+	tokens = []
+	items = string.split(" ")
+	for item in items:
+		if item == "on":
+			tokens.append(True)
+		elif item == "off":
+			tokens.append(False)
+		elif int_pattern.search(item) != None:
+			tokens.append(int(item))
+		else:
+			raise UnknownTokenError("{} is not a recognizable token".format(item))
+	return tokens
+	
+	
+class UnknownTokenError(Exception):
+	pass
+	
+	
+def parse_times(tokens, match_type = None):
+	if tokens == []: return 0
+	total_time = 0
+	index = 0
+	start_time = None
+	while index < len(tokens):
+		if type(tokens[index]) == bool:
+			start_time = tokens[index-1]
+		if type(tokens[index]) == int:
+			if start_time == None: start_time = tokens[index]
+			else: 
+				if match_type == None or tokens[index-1] == match_type:
+					total_time += calculate_time(start_time,tokens[index])
+				start_time = None
+		index += 1
+	return total_time
+	
--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 from main import calculate_time
+from main import token_times_from_string
+from main import UnknownTokenError
+from main import parse_times
 from unittest import TestCase
 
 class TestCalculateTime(TestCase):
@@ -22,3 +25,47 @@ class TestCalculateTime(TestCase):
 		
 	def test_odd_hours(self):
 		self.assertEqual(calculate_time(1011,1042), 0.52)
+		
+class TestTokenTimesFromString(TestCase):
+	def test_no_time_block(self):
+		self.assertEqual(token_times_from_string(""),[])
+	
+	def test_one_time_block(self):
+		self.assertEqual(token_times_from_string("900 on 1000"),[900, True, 1000])
+		
+	def test_multiple_successive_time_blocks(self):
+		self.assertEqual(token_times_from_string("900 on 1000 off 1100"),[900, True, 1000, False, 1100])
+	
+	def test_multiple_broken_time_blocks(self):
+		self.assertEqual(token_times_from_string("900 on 1000 off 1100 1200 on 100"),[900, True, 1000, False, 1100, 1200, True, 100])
+		
+	def test_invalid_toke(self):
+		self.assertRaises(UnknownTokenError, token_times_from_string, "900 on 1000 off hello 1100")
+		
+class TestParseTimes(TestCase):
+	def test_no_times_matching_all(self):
+		self.assertEqual(parse_times([]), 0)
+	
+	def test_one_times_matching_all(self):
+		self.assertEqual(parse_times([900, True, 1000]), 1)
+		
+	def test_two_successive_times_matching_all(self):
+		self.assertEqual(parse_times([900, True, 1000, True, 1100]), 2)
+		
+	def test_three_successive_times_matching_all(self):
+		self.assertEqual(parse_times([900, True, 1000, True, 1100, True, 100]), 4)
+		
+	def test_two_broken_times_matching_all(self):
+		self.assertEqual(parse_times([900, True, 1000, 1100, True, 1200]), 2)
+	
+	def test_three_successive_times_and_two_broken_times_matching_all(self):
+		self.assertEqual(parse_times([900, True, 1000, True, 1100, True, 100, 200, True, 300, 400, True, 500]), 6)
+		
+	def test_three_successive_times_with_one_discounted_matching_all(self):
+		self.assertEqual(parse_times([900, True, 1000, False, 1100, True, 100]), 4)
+		
+	def test_three_successive_times_with_one_discounted_matching_true(self):
+		self.assertEqual(parse_times([900, True, 1000, False, 1100, True, 100], match_type = True), 3)
+		
+	def test_three_successive_times_with_one_discounted_matching_false(self):
+		self.assertEqual(parse_times([900, True, 1000, False, 1100, True, 100], match_type = False), 1)
